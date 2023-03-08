f630395ab624351fba337612a32266b8d59d4085
rhkddms3501 <rhkddms3501@naver.com>
2023-03-08 09:52:54 +0900

    babel

---
+++ 9.babel-practice/ex04/dist/index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// 블록 스코프(es6)
+var users = [{
+  no: 1,
+  name: '마이콜',
+  email: 'michol@email.com'
+}, {
+  no: 2,
+  name: '둘리',
+  email: 'dooly@email.com'
+}];
+
+// 객체 분해(es6)
+function print(_ref) {
+  var no = _ref.no,
+    name = _ref.name,
+    email = _ref.email;
+  console.log("".concat(no, ":").concat(name, ":").concat(email));
+}
+
+// 템플릿 문자열
+for (var _i = 0, _users = users; _i < _users.length; _i++) {
+  var user = _users[_i];
+  print(user[0]);
+}
